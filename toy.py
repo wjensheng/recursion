b3a02848ad592f89b7f16f66f22c67431d604165
Jen Sheng Wong <jenshengwong@Jens-MacBook-Pro.local>
2019-08-05 09:37:48 -0500

    updated toy

--- toy.py
+++ toy.py
@@ -7,35 +7,13 @@ from easydict import EasyDict as edict
 # import models.pooling as pooling
 # from models.metric_learning import ArcMarginProduct, AddMarginProduct, AdaCos
 
-class RcicNet(nn.Module):
+class ToyNet(nn.Module):
 
-    DIVIDABLE_BY = 32
-
-    def __init__(self,
-                 n_classes,
-                 model_name='resnet50',
-                 pool='GeM',
-                 args_pooling: dict={},
-                 use_fc=False,
-                 fc_dim=512,
-                 dropout=0.0,
-                 loss_module='softmax',
-                 s=30.0,
-                 margin=0.50,
-                 ls_eps=0.0,
-                 theta_zero=0.785):
-        """
-        :param n_classes:
-        :param model_name: name of model from pretrainedmodels
-            e.g. resnet50, resnext101_32x4d, pnasnet5large
-        :param pooling: One of ('SPoC', 'MAC', 'RMAC', 'GeM', 'Rpool', 'Flatten', 'CompactBilinearPooling')
-        :param loss_module: One of ('arcface', 'cosface', 'softmax')
-        """
-        super(RcicNet, self).__init__()        
+    def __init__(self, n_classes, model_name='resnet50', use_fc=False, fc_dim=512, dropout=0.0, loss_module='softmax'):
+        super(ToyNet, self).__init__()        
 
         self.backbone = getattr(pretrainedmodels, model_name)(num_classes=1000)
 
-        # transfer weight from pretrained network
         trained_kernel = self.backbone.conv1.weight
 
         new_conv = nn.Conv2d(6, 64, kernel_size=7, stride=2, padding=3, bias=False)
@@ -47,10 +25,8 @@ class RcicNet(nn.Module):
 
         final_in_features = self.backbone.last_linear.in_features
 
-        # HACK: work around for this issue https://github.com/Cadene/pretrained-models.pytorch/issues/120
         self.backbone = nn.Sequential(*list(self.backbone.children())[:-2])
 
-        # self.pooling = getattr(pooling, pool)(**args_pooling)
         self.pooling = nn.AdaptiveAvgPool2d(1)
 
         self.use_fc = use_fc
@@ -62,14 +38,6 @@ class RcicNet(nn.Module):
             final_in_features = fc_dim
 
         self.loss_module = loss_module
-        # if loss_module == 'arcface':
-        #     self.final = ArcMarginProduct(final_in_features, n_classes,
-        #                                   s=s, m=margin, easy_margin=False, ls_eps=ls_eps)
-        # elif loss_module == 'cosface':
-        #     self.final = AddMarginProduct(final_in_features, n_classes, s=s, m=margin)
-        # elif loss_module == 'adacos':
-        #     self.final = AdaCos(final_in_features, n_classes, m=margin, theta_zero=theta_zero)
-        # else:
         self.final = nn.Linear(final_in_features, n_classes)
 
     def _init_params(self):
@@ -80,10 +48,7 @@ class RcicNet(nn.Module):
 
     def forward(self, x, label):
         feature = self.extract_feat(x)
-        if self.loss_module in ('arcface', 'cosface', 'adacos'):
-            logits = self.final(feature, label)
-        else:
-            logits = self.final(feature)
+        logits = self.final(feature)
         return logits
 
     def extract_feat(self, x):
@@ -101,36 +66,25 @@ class RcicNet(nn.Module):
 def get_model(config):
     n_classes = config.model.num_classes
     model_name = config.model.arch
-    pool = config.model.pool
-    args_pooling = {}
     use_fc = config.model.use_fc
     fc_dim = config.model.fc_dim
     dropout = config.model.dropout
     loss_module = config.model.loss_module
-    s = config.model.s
-    margin = config.model.margin    
+    
+    net = ToyNet(n_classes, model_name, use_fc, fc_dim, dropout, loss_module)
 
-    net = RcicNet(n_classes, model_name, pool, args_pooling,
-                  use_fc, fc_dim, dropout, loss_module, 
-                  s, margin)
     return net
 
 if __name__ == "__main__":
     cfg = edict()
     cfg.model = edict()
     cfg.model.arch = 'resnet18'
-    cfg.model.pool = 'GeM' 
-    cfg.model.local_whitening = False
-    cfg.model.use_fc = False
-    cfg.model.fc_dim = 512
     cfg.model.dropout = 0
     cfg.model.loss_module = 'softmax' # 'arcface', 'cosface', 'softmax'
-    cfg.model.s = 30.0
-    cfg.model.margin = 0.5
-    cfg.model.regional = False
-    cfg.model.whitening = False
-    cfg.model.image_size = 512 # resize
-    cfg.model.num_classes = 1108
+    cfg.model.use_fc = False
+    cfg.model.fc_dim = 512
+    cfg.model.image_size = 224 # resize
+    cfg.model.num_classes = 1200
     cfg.model.pretrained = True
     cfg.model.lr = 3e-4
 
