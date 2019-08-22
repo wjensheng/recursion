bca10963839f36cf0dbdeff9addf04952d9f9b18
Jen Sheng Wong <jenshengwong@Jens-MacBook-Pro.local>
2019-08-22 12:13:55 -0500

    antialias supports densenet but without pretrained weights

--- models/densenet.py
+++ models/densenet.py
@@ -9,12 +9,12 @@ from models.models_lpf import *
 __all__ = ['DenseNet', 'densenet121', 'densenet169', 'densenet201', 'densenet161']
 
 
-# model_urls = {
-#     'densenet121': 'https://download.pytorch.org/models/densenet121-a639ec97.pth',
-#     'densenet169': 'https://download.pytorch.org/models/densenet169-b2777c0a.pth',
-#     'densenet201': 'https://download.pytorch.org/models/densenet201-c1103571.pth',
-#     'densenet161': 'https://download.pytorch.org/models/densenet161-8d451a50.pth',
-# }
+model_urls = {
+    'densenet121': 'https://download.pytorch.org/models/densenet121-a639ec97.pth',
+    'densenet169': 'https://download.pytorch.org/models/densenet169-b2777c0a.pth',
+    'densenet201': 'https://download.pytorch.org/models/densenet201-c1103571.pth',
+    'densenet161': 'https://download.pytorch.org/models/densenet161-8d451a50.pth',
+}
 
 
 class _DenseLayer(nn.Sequential):
--- models/model_factory.py
+++ models/model_factory.py
@@ -43,10 +43,12 @@ class RecursionNet(nn.Module):
 
     def __init__(self, num_classes, model_name='resnet18', 
                  fc_dim=512, loss_module='softmax', antialias=True, filter_size=5):
-        super(RecursionNet, self).__init__()        
-
+        super(RecursionNet, self).__init__()
+        
+        self.backbone = globals().get(model_name)(filter_size=filter_size)
+        
         if antialias: # only supports resnet18's weights
-            self.backbone = globals().get(model_name)(filter_size=filter_size)
+            # self.backbone = globals().get(model_name)(filter_size=filter_size)
             if torch.cuda.is_available():                
                 self.backbone.load_state_dict(torch.load('weights/{0}_lpf{1}.pth.tar'.format(model_name, filter_size))['state_dict'])
             else:
@@ -63,14 +65,14 @@ class RecursionNet(nn.Module):
             self.backbone = nn.Sequential(*list(self.backbone.children())[:-2])
             
         else:
-            self.backbone = getattr(pretrainedmodels, model_name)(num_classes=1000)
+            # self.backbone = getattr(pretrainedmodels, model_name)(num_classes=1000)
 
             trained_kernel = self.backbone.features.conv0            
             self.backbone.features.conv0 = create_new_conv(trained_kernel)
 
             final_in_features = self.backbone.last_linear.in_features
 
-            self.backbone = nn.Sequential(*list(self.backbone.features)[:-1])
+            self.backbone = nn.Sequential(*list(self.backbone.features)[:-1])            
 
                         
         self.pooling = AdaptiveConcatPool2d()
@@ -126,7 +128,7 @@ def get_model(config):
     loss_module = config.loss.name
     antialias = config.model.antialias
 
-    net = RecursionNet(num_classes=num_classes, model_name=config.model.arch,
+    net = RecursionNet(num_classes=num_classes, model_name='densenet201', # config.model.arch
                        fc_dim=fc_dim, loss_module=loss_module, antialias=antialias)
                   
     return net
