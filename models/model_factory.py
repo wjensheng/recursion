976fd71547bde41b2e2908194fba28f1df29420c
Jen Sheng Wong <jenshengwong@Jens-MacBook-Pro.local>
2019-08-22 10:58:14 -0500

    refactor resnet

--- models/model_factory.py
+++ models/model_factory.py
@@ -8,6 +8,7 @@ import numpy as np
 
 import models.pooling as pooling
 from models.metric_learning import *
+from models.resnet import *
 
 class AdaptiveConcatPool2d(nn.Module):
     def __init__(self):
@@ -23,35 +24,47 @@ class Flatten(nn.Module):
 
 
 def create_new_conv(trained_kernel):
-    new_conv = nn.Conv2d(6, 64, kernel_size=7, stride=2, padding=3, bias=False)
+    trained_kernel_weights = trained_kernel.weight    
+
+    new_conv = nn.Conv2d(in_channels=6, 
+                         out_channels=trained_kernel.out_channels, 
+                         kernel_size=trained_kernel.kernel_size, 
+                         stride=trained_kernel.stride, 
+                         padding=trained_kernel.padding, 
+                         bias=False)
 
     with torch.no_grad():
-        new_conv.weight[:,:] = torch.stack([torch.mean(trained_kernel, 1)]*6, dim=1)
+        new_conv.weight[:,:] = torch.stack([torch.mean(trained_kernel_weights, 1)]*6, dim=1)
 
     return new_conv
-
-
-DEVICE = 'gpu' if torch.cuda.is_available() else 'cpu'
 
 class RecursionNet(nn.Module):
 
     def __init__(self, n_classes, model_name='resnet50', 
                  fc_dim=512, loss_module='softmax', filter_size=5):
         super(RecursionNet, self).__init__()        
-                
-        self.backbone = resnet18(filter_size=filter_size)
-        self.backbone.load_state_dict(torch.load('weights/resnet18_lpf%i.pth.tar'%filter_size)['state_dict'])
 
-        self.backbone = resnext50_32x4d()
-        self.backbone.load_state_dict(torch.load('weights/resnet18_lpf%i.pth.tar'%filter_size)['state_dict'])
+        if model_name == 'resnet18':        
+            self.backbone = resnet18(filter_size=filter_size)
+            self.expand = 1
+            if torch.cuda.is_available(): 
+                self.backbone.load_state_dict(torch.load('weights/resnet18_lpf%i.pth.tar'%filter_size)['state_dict'])
+            else:
+                self.backbone.load_state_dict(torch.load('weights/resnet18_lpf%i.pth.tar'%filter_size, map_location=torch.device('cpu'))['state_dict'])
+            
+            # if torch.cuda.is_available(): 
+            #     self.backbone.load_state_dict(torch.load('weights/resnet18_lpf%i.pth.tar'%filter_size)['state_dict'])
+            # else:
+            #     self.backbone.load_state_dict(torch.load('weights/resnet18_lpf%i.pth.tar'%filter_size, map_location=torch.device('cpu'))['state_dict'])             
+
 
         final_in_features = self.backbone.fc.in_features        
 
-        trained_kernel = self.backbone.conv1.weight            
+        # change first filter
+        trained_kernel = self.backbone.conv1
         self.backbone.conv1 = create_new_conv(trained_kernel)
-        
-        self.backbone = nn.Sequential(*list(self.backbone.children())[:-2])
-        self.expand = 1
+
+        self.backbone = nn.Sequential(*list(self.backbone.children())[:-2])        
             
         self.pooling = AdaptiveConcatPool2d()
         self.flatten = Flatten()        
@@ -96,328 +109,12 @@ class RecursionNet(nn.Module):
         x = self.pooling(x)
         x = self.flatten(x)        
         x = self.bn1(x)
-        x = self.dropout1(x)
+        # x = self.dropout1(x)
         x = self.fc1(x)
         x = self.relu(x)        
         x = self.bn2(x)
         x = self.dropout2(x)
         return x
-
-
-class Downsample(nn.Module):
-    def __init__(self, pad_type='reflect', filt_size=3, stride=2, channels=None, pad_off=0):
-        super(Downsample, self).__init__()
-        self.filt_size = filt_size
-        self.pad_off = pad_off
-        self.pad_sizes = [int(1.*(filt_size-1)/2), int(np.ceil(1.*(filt_size-1)/2)), int(1.*(filt_size-1)/2), int(np.ceil(1.*(filt_size-1)/2))]
-        self.pad_sizes = [pad_size+pad_off for pad_size in self.pad_sizes]
-        self.stride = stride
-        self.off = int((self.stride-1)/2.)
-        self.channels = channels
-
-        # print('Filter size [%i]'%filt_size)
-        if(self.filt_size==1):
-            a = np.array([1.,])
-        elif(self.filt_size==2):
-            a = np.array([1., 1.])
-        elif(self.filt_size==3):
-            a = np.array([1., 2., 1.])
-        elif(self.filt_size==4):    
-            a = np.array([1., 3., 3., 1.])
-        elif(self.filt_size==5):    
-            a = np.array([1., 4., 6., 4., 1.])
-        elif(self.filt_size==6):    
-            a = np.array([1., 5., 10., 10., 5., 1.])
-        elif(self.filt_size==7):    
-            a = np.array([1., 6., 15., 20., 15., 6., 1.])
-
-        filt = torch.Tensor(a[:,None]*a[None,:])
-        filt = filt/torch.sum(filt)
-        self.register_buffer('filt', filt[None,None,:,:].repeat((self.channels,1,1,1)))
-
-        self.pad = get_pad_layer(pad_type)(self.pad_sizes)
-
-    def forward(self, inp):
-        if(self.filt_size==1):
-            if(self.pad_off==0):
-                return inp[:,:,::self.stride,::self.stride]    
-            else:
-                return self.pad(inp)[:,:,::self.stride,::self.stride]
-        else:
-            return F.conv2d(self.pad(inp), self.filt, stride=self.stride, groups=inp.shape[1])
-
-def get_pad_layer(pad_type):
-    if(pad_type in ['refl','reflect']):
-        PadLayer = nn.ReflectionPad2d
-    elif(pad_type in ['repl','replicate']):
-        PadLayer = nn.ReplicationPad2d
-    elif(pad_type=='zero'):
-        PadLayer = nn.ZeroPad2d
-    else:
-        print('Pad type [%s] not recognized'%pad_type)
-    return PadLayer
-
-
-class Downsample1D(nn.Module):
-    def __init__(self, pad_type='reflect', filt_size=3, stride=2, channels=None, pad_off=0):
-        super(Downsample1D, self).__init__()
-        self.filt_size = filt_size
-        self.pad_off = pad_off
-        self.pad_sizes = [int(1. * (filt_size - 1) / 2), int(np.ceil(1. * (filt_size - 1) / 2))]
-        self.pad_sizes = [pad_size + pad_off for pad_size in self.pad_sizes]
-        self.stride = stride
-        self.off = int((self.stride - 1) / 2.)
-        self.channels = channels
-
-        # print('Filter size [%i]' % filt_size)
-        if(self.filt_size == 1):
-            a = np.array([1., ])
-        elif(self.filt_size == 2):
-            a = np.array([1., 1.])
-        elif(self.filt_size == 3):
-            a = np.array([1., 2., 1.])
-        elif(self.filt_size == 4):
-            a = np.array([1., 3., 3., 1.])
-        elif(self.filt_size == 5):
-            a = np.array([1., 4., 6., 4., 1.])
-        elif(self.filt_size == 6):
-            a = np.array([1., 5., 10., 10., 5., 1.])
-        elif(self.filt_size == 7):
-            a = np.array([1., 6., 15., 20., 15., 6., 1.])
-
-        filt = torch.Tensor(a)
-        filt = filt / torch.sum(filt)
-        self.register_buffer('filt', filt[None, None, :].repeat((self.channels, 1, 1)))
-
-        self.pad = get_pad_layer_1d(pad_type)(self.pad_sizes)
-
-    def forward(self, inp):
-        if(self.filt_size == 1):
-            if(self.pad_off == 0):
-                return inp[:, :, ::self.stride]
-            else:
-                return self.pad(inp)[:, :, ::self.stride]
-        else:
-            return F.conv1d(self.pad(inp), self.filt, stride=self.stride, groups=inp.shape[1])
-
-
-def get_pad_layer_1d(pad_type):
-    if(pad_type in ['refl', 'reflect']):
-        PadLayer = nn.ReflectionPad1d
-    elif(pad_type in ['repl', 'replicate']):
-        PadLayer = nn.ReplicationPad1d
-    elif(pad_type == 'zero'):
-        PadLayer = nn.ZeroPad1d
-    else:
-        print('Pad type [%s] not recognized' % pad_type)
-    return PadLayer
-
-    
-def conv3x3(in_planes, out_planes, stride=1, groups=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                 padding=1, groups=groups, bias=False)
-
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, norm_layer=None, filter_size=1):
-        super(BasicBlock, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        if groups != 1:
-            raise ValueError('BasicBlock only supports groups=1')
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        if(stride==1):
-            self.conv2 = conv3x3(planes,planes)
-        else:
-            self.conv2 = nn.Sequential(Downsample(filt_size=filter_size, stride=stride, channels=planes),
-                conv3x3(planes, planes),)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, norm_layer=None, filter_size=1):
-        super(Bottleneck, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv1x1(inplanes, planes)
-        self.bn1 = norm_layer(planes)
-        self.conv2 = conv3x3(planes, planes, groups) # stride moved
-        self.bn2 = norm_layer(planes)
-        if(stride==1):
-            self.conv3 = conv1x1(planes, planes * self.expansion)
-        else:
-            self.conv3 = nn.Sequential(Downsample(filt_size=filter_size, stride=stride, channels=planes),
-                conv1x1(planes, planes * self.expansion))
-        self.bn3 = norm_layer(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class ResNet(nn.Module):
-
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
-                 groups=1, width_per_group=64, norm_layer=None, filter_size=1, pool_only=True):
-        super(ResNet, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        planes = [int(width_per_group * groups * 2 ** i) for i in range(4)]
-        self.inplanes = planes[0]
-
-        if(pool_only):
-            self.conv1 = nn.Conv2d(3, planes[0], kernel_size=7, stride=2, padding=3, bias=False)
-        else:
-            self.conv1 = nn.Conv2d(3, planes[0], kernel_size=7, stride=1, padding=3, bias=False)
-        self.bn1 = norm_layer(planes[0])
-        self.relu = nn.ReLU(inplace=True)
-
-        if(pool_only):
-            self.maxpool = nn.Sequential(*[nn.MaxPool2d(kernel_size=2, stride=1), 
-                Downsample(filt_size=filter_size, stride=2, channels=planes[0])])
-        else:
-            self.maxpool = nn.Sequential(*[Downsample(filt_size=filter_size, stride=2, channels=planes[0]), 
-                nn.MaxPool2d(kernel_size=2, stride=1), 
-                Downsample(filt_size=filter_size, stride=2, channels=planes[0])])
-
-        self.layer1 = self._make_layer(block, planes[0], layers[0], groups=groups, norm_layer=norm_layer)
-        self.layer2 = self._make_layer(block, planes[1], layers[1], stride=2, groups=groups, norm_layer=norm_layer, filter_size=filter_size)
-        self.layer3 = self._make_layer(block, planes[2], layers[2], stride=2, groups=groups, norm_layer=norm_layer, filter_size=filter_size)
-        self.layer4 = self._make_layer(block, planes[3], layers[3], stride=2, groups=groups, norm_layer=norm_layer, filter_size=filter_size)
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(planes[3] * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                if(m.in_channels!=m.out_channels or m.out_channels!=m.groups or m.bias is not None):
-                    # don't want to reinitialize downsample layers, code assuming normal conv layers will not have these characteristics
-                    nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                else:
-                    print('Not initializing')
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
-
-    def _make_layer(self, block, planes, blocks, stride=1, groups=1, norm_layer=None, filter_size=1):
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            # downsample = nn.Sequential(
-            #     conv1x1(self.inplanes, planes * block.expansion, stride, filter_size=filter_size),
-            #     norm_layer(planes * block.expansion),
-            # )
-
-            downsample = [Downsample(filt_size=filter_size, stride=stride, channels=self.inplanes),] if(stride !=1) else []
-            downsample += [conv1x1(self.inplanes, planes * block.expansion, 1),
-                norm_layer(planes * block.expansion)]
-            # print(downsample)
-            downsample = nn.Sequential(*downsample)
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, groups, norm_layer, filter_size=filter_size))
-        self.inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(self.inplanes, planes, groups=groups, norm_layer=norm_layer, filter_size=filter_size))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-
-        return x        
-
-
-def resnet18(pretrained=False, filter_size=1, pool_only=True, **kwargs):
-    """Constructs a ResNet-18 model.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = ResNet(BasicBlock, [2, 2, 2, 2], filter_size=filter_size, pool_only=pool_only, **kwargs)
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
-    return model
-
-
-def resnext50_32x4d(pretrained=False, filter_size=1, pool_only=True, **kwargs):
-    model = ResNet(Bottleneck, [3, 4, 6, 3], groups=4, width_per_group=32, filter_size=filter_size, pool_only=pool_only, **kwargs)
-    return model
-
 
 def get_model(config):
     n_classes = config.model.num_classes
@@ -425,7 +122,7 @@ def get_model(config):
     fc_dim = config.model.fc_dim
     loss_module = config.loss.name
 
-    net = RecursionNet(n_classes=n_classes, model_name=model_name,
+    net = RecursionNet(n_classes=n_classes, model_name=config.model.arch,
                        fc_dim=fc_dim, loss_module=loss_module)
                   
     return net
