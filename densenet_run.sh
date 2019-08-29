09127b7890d05d703102846ac448d6a0d3b0243a
Jen Sheng Wong <jenshengwong@Jens-MacBook-Pro.local>
2019-08-29 00:47:55 -0400

    separated resnet and densenet

--- densenet_run.sh
+++ densenet_run.sh
@@ -1,24 +1,6 @@
-# NOTE:
-# This script contains 12 experiments for 
-# each architecture (resnet50 & densenet201)
-# to find the best loss function.
-# Arguments ls and bestfitting only needed for loss=arcface or cosface.
-
-# After running all experiments, i.e., script finishes running,
-# tune learning rate for the best loss function. 
-# Find the best num_grad_acc, from 0, 2, 4, 8
-# Optim_wd can set to 0.01 once the best num_grad_acc is found.
-
-# Other stuff to be aware of:
-# Try tuning for batch_size of 16, 24, 32.
-# Use smaller image size to speed up experiments.
-# Increase step_size if loss flattens out and 
-# decrease step_size if loss remains constant
-# Note that RAdam, a.k.a 'radam' is not compatible with 
-# CosineAnnealing, a.k.a 'cosine' as the scheduler
-
+# =========================== densenet ============================
 # default: cross_entropy
-python3 train.py --config=configs/resnet50.yml \
+python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
                  --batch_size=32 \
                  --image_size=256 \
@@ -32,7 +14,7 @@ python3 train.py --config=configs/resnet50.yml \
                  --step_size=40
 
 # ls_cross_entropy
-python3 train.py --config=configs/resnet50.yml \
+python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
                  --batch_size=32 \
                  --image_size=256 \
@@ -46,9 +28,9 @@ python3 train.py --config=configs/resnet50.yml \
                  --step_size=40
 
 # arcface, ls=false, bestfitting=false
-python3 train.py --config=configs/resnet50.yml \
+python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
-                 --batch_size=32 \ 
+                 --batch_size=32 \
                  --image_size=256 \
                  --loss=arcface \
                  --optim_lr=0.0005 \
@@ -60,7 +42,7 @@ python3 train.py --config=configs/resnet50.yml \
                  --step_size=40                                  
 
 # arcface, ls=true, bestfitting=false
-python3 train.py --config=configs/resnet50.yml \
+python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
                  --batch_size=32 \
                  --image_size=256 \
@@ -74,7 +56,7 @@ python3 train.py --config=configs/resnet50.yml \
                  --step_size=40                                                   
 
 # arcface, ls=false, bestfitting=true
-python3 train.py --config=configs/resnet50.yml \
+python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
                  --batch_size=32 \
                  --image_size=256 \
@@ -88,7 +70,7 @@ python3 train.py --config=configs/resnet50.yml \
                  --step_size=40                                                   
 
 # arcface, ls=true, bestfitting=true
-python3 train.py --config=configs/resnet50.yml \
+python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
                  --batch_size=32 \
                  --image_size=256 \
@@ -102,7 +84,7 @@ python3 train.py --config=configs/resnet50.yml \
                  --step_size=40                                                   
 
 # cosface, ls=false, bestfitting=false
-python3 train.py --config=configs/resnet50.yml \
+python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
                  --batch_size=32 \
                  --image_size=256 \
@@ -116,7 +98,7 @@ python3 train.py --config=configs/resnet50.yml \
                  --step_size=40                                  
 
 # cosface, ls=true, bestfitting=false
-python3 train.py --config=configs/resnet50.yml \
+python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
                  --batch_size=32 \
                  --image_size=256 \
@@ -130,7 +112,7 @@ python3 train.py --config=configs/resnet50.yml \
                  --step_size=40                                                   
 
 # cosface, ls=false, bestfitting=true
-python3 train.py --config=configs/resnet50.yml \
+python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
                  --batch_size=32 \
                  --image_size=256 \
@@ -144,7 +126,7 @@ python3 train.py --config=configs/resnet50.yml \
                  --step_size=40                                                   
 
 # cosface, ls=true, bestfitting=true
-python3 train.py --config=configs/resnet50.yml \
+python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
                  --batch_size=32 \
                  --image_size=256 \
