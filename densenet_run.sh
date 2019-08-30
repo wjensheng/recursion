773a9b8187cdca422fbc4a642c41740ffa0e5036
Jen Sheng Wong <jenshengwong@Jens-MacBook-Pro.local>
2019-08-30 15:35:53 -0400

    change densenet lr

--- densenet_run.sh
+++ densenet_run.sh
@@ -11,41 +11,12 @@
 #                  --ls=true \
 #                  --bestfitting=true \
 #                  --scheduler=step \
-#                  --step_size=40                                                                  
-
-# arcface, ls=false, bestfitting=true
-python3 train.py --config=configs/densenet201.yml \
-                 --num_epochs=60 \
-                 --batch_size=24 \
-                 --image_size=512 \
-                 --loss=arcface \
-                 --optim_lr=0.0003 \
-                 --optim_wd=0 \
-                 --num_grad_acc=0 \
-                 --ls=false \
-                 --bestfitting=true \
-                 --scheduler=step \
-                 --step_size=40                                                   
-
-# arcface, ls=true, bestfitting=true
-python3 train.py --config=configs/densenet201.yml \
-                 --num_epochs=60 \
-                 --batch_size=24 \
-                 --image_size=512 \
-                 --loss=arcface \
-                 --optim_lr=0.0003 \
-                 --optim_wd=0 \
-                 --num_grad_acc=0 \
-                 --ls=true \
-                 --bestfitting=true \
-                 --scheduler=step \
-                 --step_size=40                                                   
-                                               
+#                  --step_size=40
 
 # cosface, ls=false, bestfitting=true
 python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
-                 --batch_size=24 \
+                 --batch_size=32 \
                  --image_size=512 \
                  --loss=cosface \
                  --optim_lr=0.0003 \
@@ -54,18 +25,47 @@ python3 train.py --config=configs/densenet201.yml \
                  --ls=false \
                  --bestfitting=true \
                  --scheduler=step \
-                 --step_size=40                                                   
+                 --step_size=40
 
-# cosface, ls=true, bestfitting=true
+# cosface, ls=false, bestfitting=true
 python3 train.py --config=configs/densenet201.yml \
                  --num_epochs=60 \
-                 --batch_size=24 \
+                 --batch_size=32 \
                  --image_size=512 \
                  --loss=cosface \
-                 --optim_lr=0.0003 \
+                 --optim_lr=0.0002 \
                  --optim_wd=0 \
                  --num_grad_acc=0 \
-                 --ls=true \
+                 --ls=false \
                  --bestfitting=true \
                  --scheduler=step \
                  --step_size=40
+                 
+# cosface, ls=false, bestfitting=true
+python3 train.py --config=configs/densenet201.yml \
+                 --num_epochs=60 \
+                 --batch_size=32 \
+                 --image_size=512 \
+                 --loss=cosface \
+                 --optim_lr=0.0005 \
+                 --optim_wd=0 \
+                 --num_grad_acc=0 \
+                 --ls=false \
+                 --bestfitting=true \
+                 --scheduler=step \
+                 --step_size=40                 
+
+
+# cosface, ls=false, bestfitting=true
+python3 train.py --config=configs/densenet201.yml \
+                 --num_epochs=60 \
+                 --batch_size=32 \
+                 --image_size=512 \
+                 --loss=cosface \
+                 --optim_lr=0.0007 \
+                 --optim_wd=0 \
+                 --num_grad_acc=0 \
+                 --ls=false \
+                 --bestfitting=true \
+                 --scheduler=step \
+                 --step_size=40                                  
