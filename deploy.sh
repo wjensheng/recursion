a08b93e89c515a942404690417135b1bd46aca1b
Jen Sheng Wong <jenshengwong@Jens-MacBook-Pro.local>
2019-08-31 10:16:34 -0400

    tune lr

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ python3 train.py --config=configs/resnet101.yml \
                  --batch_size=32 \
                  --image_size=256 \
                  --loss=cosface \
-                 --optim_lr=0.0005 \
+                 --optim_lr=0.0002 \
                  --optim_wd=0 \
                  --num_grad_acc=0 \
                  --ls=false \
@@ -12,16 +12,44 @@ python3 train.py --config=configs/resnet101.yml \
                  --scheduler=step \
                  --step_size=40 
 
-# cosface, ls=false, bestfitting=false
+# cosface, ls=false, bestfitting=true
 python3 train.py --config=configs/resnet101.yml \
                  --num_epochs=60 \
                  --batch_size=32 \
                  --image_size=256 \
                  --loss=cosface \
-                 --optim_lr=0.0005 \
+                 --optim_lr=0.0003 \
                  --optim_wd=0 \
                  --num_grad_acc=0 \
                  --ls=false \
-                 --bestfitting=false \
+                 --bestfitting=true \
                  --scheduler=step \
-                 --step_size=40
+                 --step_size=40 
+
+# cosface, ls=false, bestfitting=true
+python3 train.py --config=configs/resnet101.yml \
+                 --num_epochs=60 \
+                 --batch_size=32 \
+                 --image_size=256 \
+                 --loss=cosface \
+                 --optim_lr=0.0007 \
+                 --optim_wd=0 \
+                 --num_grad_acc=0 \
+                 --ls=false \
+                 --bestfitting=true \
+                 --scheduler=step \
+                 --step_size=40 
+
+# cosface, ls=false, bestfitting=true
+python3 train.py --config=configs/resnet101.yml \
+                 --num_epochs=60 \
+                 --batch_size=32 \
+                 --image_size=256 \
+                 --loss=cosface \
+                 --optim_lr=0.001 \
+                 --optim_wd=0 \
+                 --num_grad_acc=0 \
+                 --ls=false \
+                 --bestfitting=true \
+                 --scheduler=step \
+                 --step_size=40 
