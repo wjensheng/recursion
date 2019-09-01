bee6dcb35b6aefdda3ac6965309f9535b842cc05
Jen Sheng Wong <jenshengwong@Jens-MacBook-Pro.local>
2019-09-01 07:57:21 -0400

    norm wrt to cell types

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 # cosface, ls=false, bestfitting=true
 python3 train.py --config=configs/densenet121.yml \
                  --num_epochs=60 \
-                 --batch_size=32 \
+                 --batch_size=16 \
                  --image_size=512 \
                  --loss=cosface \
-                 --optim_lr=0.0003 \
+                 --optim_lr=0.0005 \
                  --optim_wd=0 \
                  --num_grad_acc=0 \
                  --ls=false \
@@ -15,7 +15,7 @@ python3 train.py --config=configs/densenet121.yml \
 # cosface, ls=false, bestfitting=true
 python3 train.py --config=configs/densenet121.yml \
                  --num_epochs=60 \
-                 --batch_size=32 \
+                 --batch_size=16 \
                  --image_size=512 \
                  --loss=cosface \
                  --optim_lr=0.0005 \
@@ -23,47 +23,6 @@ python3 train.py --config=configs/densenet121.yml \
                  --num_grad_acc=0 \
                  --ls=false \
                  --bestfitting=true \
-                 --scheduler=step \
-                 --step_size=30
-
-# cosface, ls=false, bestfitting=true
-python3 train.py --config=configs/densenet121.yml \
-                 --num_epochs=60 \
-                 --batch_size=32 \
-                 --image_size=512 \
-                 --loss=cosface \
-                 --optim_lr=0.0007 \
-                 --optim_wd=0 \
-                 --num_grad_acc=0 \
-                 --ls=false \
-                 --bestfitting=true \
-                 --scheduler=step \
-                 --step_size=30
-
-# # cosface, ls=false, bestfitting=true
-# python3 train.py --config=configs/resnet101.yml \
-#                  --num_epochs=60 \
-#                  --batch_size=32 \
-#                  --image_size=320 \
-#                  --loss=cosface \
-#                  --optim_lr=0.002 \
-#                  --optim_wd=0 \
-#                  --num_grad_acc=0 \
-#                  --ls=false \
-#                  --bestfitting=true \
-#                  --scheduler=step \
-#                  --step_size=30 
-
-# # cosface, ls=false, bestfitting=true
-# python3 train.py --config=configs/resnet101.yml \
-#                  --num_epochs=60 \
-#                  --batch_size=32 \
-#                  --image_size=256 \
-#                  --loss=cosface \
-#                  --optim_lr=0.001 \
-#                  --optim_wd=0 \
-#                  --num_grad_acc=0 \
-#                  --ls=false \
-#                  --bestfitting=true \
-#                  --scheduler=step \
-#                  --step_size=40 
+                 --scheduler=cosine \
+                 --eta_min=0.00005 \
+                 --t_max=138
