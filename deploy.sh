d0e66974508b6a8c36534b0967b2d246044f6145
Jen Sheng Wong <jenshengwong@Jens-MacBook-Pro.local>
2019-09-02 11:01:39 -0400

    trying adamw

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,24 @@
+# activate virtual
+actVir
+
+# unlock
+chmod u+rwx data/pixel_stats.csv
+
+# cosface, ls=false, bestfitting=true
+python3 train.py --config=configs/resnet101.yml \
+                 --num_epochs=60 \
+                 --batch_size=24 \
+                 --image_size=320 \
+                 --loss=cosface \
+                 --optim_lr=0.0003 \
+                 --optim_wd=0 \
+                 --num_grad_acc=0 \
+                 --ls=false \
+                 --bestfitting=true \
+                 --scheduler=cosine \
+                 --t_max=184
+                 --eta_min=0.00003
+
 # cosface, ls=false, bestfitting=true
 python3 train.py --config=configs/resnet101.yml \
                  --num_epochs=60 \
@@ -9,8 +30,9 @@ python3 train.py --config=configs/resnet101.yml \
                  --num_grad_acc=0 \
                  --ls=false \
                  --bestfitting=true \
-                 --scheduler=step \
-                 --step_size=40
+                 --scheduler=cosine \
+                 --t_max=184
+                 --eta_min=0.00005
 
 # cosface, ls=false, bestfitting=true
 python3 train.py --config=configs/resnet101.yml \
@@ -23,8 +45,9 @@ python3 train.py --config=configs/resnet101.yml \
                  --num_grad_acc=0 \
                  --ls=false \
                  --bestfitting=true \
-                 --scheduler=step \
-                 --step_size=40
+                 --scheduler=cosine \
+                 --t_max=184
+                 --eta_min=0.00007
 
 # cosface, ls=false, bestfitting=true
 python3 train.py --config=configs/resnet101.yml \
@@ -37,5 +60,6 @@ python3 train.py --config=configs/resnet101.yml \
                  --num_grad_acc=0 \
                  --ls=false \
                  --bestfitting=true \
-                 --scheduler=step \
-                 --step_size=40                                  
+                 --scheduler=cosine \
+                 --t_max=184
+                 --eta_min=0.0001
